99c5ab886469a029826d67d9af78b398a4ce994e
Guilherme Espindola <98782954+guilhermeen@users.noreply.github.com>
2022-05-07 15:56:52 -0300

    Ex 003
    Exercicio N003

---
+++ ex3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main (String[] args){
+  Scanner n = new Scanner(System.in);
+
+    int num = n.nextInt();
+    long fatorial = 1;
+    for(int i=1 ; i <= num ;i++){
+      fatorial = fatorial * i;
+  }
+
+  System.out.println("O fatorial de " + num +" é igual a: " + fatorial);
+  }
+}
