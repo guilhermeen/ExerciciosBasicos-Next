990e3d37bca2f7274ab2d37b6586b3e9a209910e
Guilherme Espindola <98782954+guilhermeen@users.noreply.github.com>
2022-05-10 15:22:47 -0300

    Add files via upload

---
+++ ex005App.java
@@ -0,0 +1,15 @@
+class App {
+    public static void app(String[] args) {
+        Empregado num1[] = new Empregado[1];
+        num1[0] = new Empregado("Guilherme", "Espindola", int 1200); 
+
+        //Nome e sobrenomo do funcionario
+        System.out.println(getIdentidade)
+        //Salario inicial 
+        System.out.println(getSalario)
+        //Chamada do metodo para aumentar o salario
+        num1[0].aumento(1200);
+        //Novo salario
+        System.out.println(getSalario);
+    }
+} 
---
+++ ex005ClassEmpregado.java
@@ -0,0 +1,67 @@
+public class Empregado(){
+    private String nome;
+    private String sobrenome;
+    private int salario;
+    public String identidade;
+
+    //construtor inicializando todos os atributos
+    public Empregado(String nome, String sobrenome, int salario){ 
+        this.nome =nome;
+        this.sobrenome=sobrenome;
+        this.salario=salario;
+    }
+    //construtor sem inicializar nenhum atributo
+    //public Empregado(){ 
+      
+    //}
+
+
+    //Metodo para aumentar o salario em 10%
+    public void aumento(salario){
+        int novoSalario=0;
+        novoSalario=this.salario + (this.salario*0.10)
+        salario=novoSalario;
+    }
+
+
+    //Metodo para definir variavel como o nome +sobrenome
+    public String getIdentidade(){
+        return identidade;
+    }
+    public void setIdentidade(){
+        identidade = (getNome+getSobrenome);
+    }
+    
+    
+    
+    //Metodos getters e setters 
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome =nome;
+    }
+
+    public String getSobrenome(){
+        return sobrenome;
+    }
+
+    public void setSobrenome(){
+        this.sobrenome=sobrenome;
+    }
+
+    public int getSalario(){
+        return salario;
+    }
+
+    public void setSalario(){
+        this.salario=salario;
+    }
+
+
+
+
+
+
+}
